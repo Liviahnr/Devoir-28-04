09e74f29c38c89804c255f0ca396218c9bcecc7b
Liviahnr <163434109+Liviahnr@users.noreply.github.com>
2024-05-04 13:58:13 +0200

    Update Agent.js

--- front/Agent.js
+++ front/Agent.js
@@ -10,25 +10,10 @@ fetch(`http://localhost:3000/agents/${id}`)
            <h1>${data.name}</h1>
 
 					<a href="index.html"> Back </a>
-          <a href="Agent.html?id=${data.id}">Rename</a>
+          <a href="update.html?id=${data.id}">Update</a>
         <button onclick="deleteAgent()">Delete</button>
                 `;
   });
-
-const updateAgent = async () => {
-  let agent = document.querySelector('input[name="agent"]').value;
-
-  let response = await fetch(`http://localhost:3000/agents/${id}`, {
-    method: "PUT",
-
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ name: agent }),
-  });
-
-  window.location.href = "index.html";
-};
 
 const deleteAgent = async () => {
   let response = await fetch(`http://localhost:3000/agents/${id}`, {
